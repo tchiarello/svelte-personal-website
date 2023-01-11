3efefd15ce5750f91244cf1d4edf47e0495a931f
Tatiane Chiarello <tatichiarello@gmail.com>
2023-01-11 18:36:48 +0100

    choro: delete unused code

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,13 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
+	// import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
 	<div class="corner">
 		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+			<!-- <img src={logo} alt="SvelteKit" /> -->
 		</a>
 	</div>
 
