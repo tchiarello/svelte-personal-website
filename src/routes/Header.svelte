8f4bfd55b5018d8f027a0a4efc7ee6cf9cd37ba4
Tatiane Chiarello <tatichiarello@gmail.com>
2023-01-16 16:04:54 +0100

    fix: add social medias on header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,31 @@
 <script>
 	import { page } from '$app/stores';
+	import github from '$lib/images/github.svg';
+	import linkedin from '$lib/images/linkedin.svg';
 </script>
 
 <header>
 	<nav>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a class="link" href="/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/projects' ? 'page' : undefined}>
-				<a href="/projects">Projects</a>
+				<a class="link" href="/projects">Projects</a>
+			</li>
+		</ul>
+		<ul>
+			<li>
+				<a href="https://github.com/tchiarello" target="_blank" rel="noreferrer">
+					<img src={github} alt="GitHub icon" />
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://www.linkedin.com/in/tatiane-chiarello/?locale=en_US"
+					target="_blank"
+					rel="noreferrer"><img src={linkedin} alt="LinkedIn icon" /></a
+				>
 			</li>
 		</ul>
 	</nav>
@@ -19,12 +35,15 @@
 	header {
 		box-sizing: border-box;
 		position: fixed;
-		display: flex;
-		justify-content: flex-end;
 		width: 100%;
-		background-color: var(--color-bg-1);
-		padding: 24px 16px;
-		box-shadow: 0 -30px 30px 15px rgb(0 0 0 / 70%);
+		background-color: var(--color-bg);
+		border-bottom: 1px solid var(--color-theme-2);
+		padding: 24px;
+
+		nav {
+			display: flex;
+			justify-content: space-between;
+		}
 
 		ul {
 			padding: 0;
@@ -36,18 +55,23 @@
 			list-style: none;
 		}
 
-		a {
+		.link {
 			position: relative;
-			color: var(--color-text);
+			color: var(--color-theme-1);
 			font-weight: 700;
 			font-size: 0.8rem;
 			text-transform: uppercase;
-			letter-spacing: 3px;
+			letter-spacing: 2px;
+			transition: all 0.8s;
 		}
 
-		a::before {
-			content: "";
-			background-color: var(--color-theme-1);
+		.link:hover {
+			color: var(--color-theme-2);
+		}
+
+		.link::before {
+			content: '';
+			background-color: var(--color-theme-2);
 			transition: 0.4s ease-out;
 			width: 0%;
 			height: 2px;
@@ -57,7 +81,7 @@
 			bottom: -3px;
 		}
 
-		a:hover::before {
+		.link:hover::before {
 			width: 100%;
 		}
 	}
