a29716920013cfcb0e54f5eb6050bec3a4d7c275
Tatiane Chiarello <tatichiarello@gmail.com>
2023-01-16 16:05:23 +0100

    feat: create footer

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+	<span>Built in Berlin. Powered by SvelteKit</span>
+</footer>
+
+<style lang="scss">
+	footer {
+		display: flex;
+		justify-content: center;
+		background-color: var(--color-theme-2);
+		color: var(--color-bg);
+		padding: 24px;
+	}
+</style>
